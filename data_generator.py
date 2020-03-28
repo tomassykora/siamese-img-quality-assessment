3a2f2c433eede29d9f3f8448e57633d6af59d03a
Tomas Sykora <tms.sykora@gmail.com>
2020-03-28 19:49:03 +0100

    Better image pairs creation

--- data_generator.py
+++ data_generator.py
@@ -11,51 +11,49 @@ from utils import get_preprocessed_patches
 
 class DataGenerator:
 
-    def __init__(self, dataset_path, batch_size=128, min_iqa_difference=4.0, num_patches_for_image=8, class_margin=0.3):
+    def __init__(self, dataset_path, batch_size=128, min_iqa_difference=10.0, num_patches_for_image=8):
+        assert batch_size % num_patches_for_image == 0
+
         self.batch_size = batch_size
         self.dataset_path = dataset_path
         self.min_iqa_difference = min_iqa_difference
         self.num_patches_for_image = num_patches_for_image
-        self.class_margin = class_margin
 
-        seed(11)
         self.images, self.test_images = self._load_live2_db()
+        print(f'LOG: number of train images: {len(self.images)}')
 
 
     def generate(self):
         while 1:
-            iqa_batch_a, iqa_batch_b, y_true = [], [], []
-            while len(iqa_batch_a) < self.batch_size:
-                greater_iqa_img = self.images[randrange(len(self.images))]
-                lower_iqa_img = self.images[randrange(len(self.images))]
-                while (
-                    greater_iqa_img == lower_iqa_img or
-                    abs(greater_iqa_img['iqa'] - lower_iqa_img['iqa']) < self.min_iqa_difference
-                ):
-                    lower_iqa_img = self.images[randrange(len(self.images))]
+            starting_new_batch = True
+            for img_a in self.images:
+                for img_b in self.images:
+                    if (
+                        img_a == img_b or
+                        abs(img_a['iqa'] - img_b['iqa']) < self.min_iqa_difference
+                    ):
+                        continue
 
-                iqa_batch_a += get_preprocessed_patches(greater_iqa_img['path'])
-                iqa_batch_b   += get_preprocessed_patches(lower_iqa_img['path'])
-                y_true += [np.array([1.0, 0.0]) if greater_iqa_img['iqa'] > lower_iqa_img['iqa'] else np.array([0.0, 1.0])
-                            for i in range(self.num_patches_for_image)]
+                    if starting_new_batch:
+                        iqa_batch_a, iqa_batch_b, y_true = [], [], []
+                        starting_new_batch = False
 
-            zipped_batches = list(zip(iqa_batch_a, iqa_batch_b, y_true))
-            shuffle(zipped_batches)
-            iqa_batch_a, iqa_batch_b, y_true = zip(*zipped_batches)
+                    iqa_batch_a += get_preprocessed_patches(img_a['path'])
+                    iqa_batch_b += get_preprocessed_patches(img_b['path'])
 
-            # y_true += [1.0 if greater_iqa_img['iqa'] > lower_iqa_img['iqa'] else 0.0 for i in range(self.num_patches_for_image)]
-            # for batch in zip(iqa_batch_a, iqa_batch_b):
-            #     g, l = batch
-            #     print(g.shape, l.shape)
-            #     y_true.append(1.0 if g['iqa'] > l['iqa'] else 0.0)
+                    y_true += [np.array([1.0, 0.0]) if img_a['iqa'] > img_b['iqa'] else np.array([0.0, 1.0])
+                                for i in range(self.num_patches_for_image)]
 
-            iqa_batch_a = np.moveaxis(np.array(iqa_batch_a), 1, 3)
-            iqa_batch_b = np.moveaxis(np.array(iqa_batch_b), 1, 3)
+                    if len(iqa_batch_a) >= self.batch_size:
+                        zipped_batches = list(zip(iqa_batch_a, iqa_batch_b, y_true))
+                        shuffle(zipped_batches)
+                        iqa_batch_a, iqa_batch_b, y_true = zip(*zipped_batches)
 
-            # the siamese architecture uses only the batch, not the second (y) value, thus setting to np.ones
-            # yield [iqa_batch_a, iqa_batch_b], np.ones((self.batch_size, 1))
-            # yield [iqa_batch_a, iqa_batch_b], np.array(y_true).reshape(len(np.array(y_true)), 2)
-            yield [iqa_batch_a, iqa_batch_b], np.array(y_true)
+                        iqa_batch_a = np.moveaxis(np.array(iqa_batch_a), 1, 3)
+                        iqa_batch_b = np.moveaxis(np.array(iqa_batch_b), 1, 3)
+
+                        starting_new_batch = True
+                        yield [iqa_batch_a, iqa_batch_b], np.array(y_true)
 
 
     def _load_live2_db(self):
@@ -70,6 +68,7 @@ class DataGenerator:
                 'path': join(distortion_path, img_description.split()[1]),
                 'iqa': float(img_description.split()[2])
             } for img_description in lines if not img_description.isspace() and float(img_description.split()[2]) != 0.0]
+        seed(11)
         shuffle(images)
 
         # Splitting in 80/20% ratio
@@ -89,6 +88,7 @@ class DataGenerator:
                 'path': path.join(self.dataset_path, 'distorted_images', inf.split()[1]),
                 'iqa': float(inf.split()[0])
             })
+        seed(11)
         shuffle(images)
 
         # Splitting in 80/20% ratio
